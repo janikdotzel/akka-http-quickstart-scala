67e483d90a73ce14e9206bbfe9ebaec20d643718
Janik Dotzel <janikdotzel96@gmail.com>
2023-05-10 13:50:04 +0200

    optimizing json example

--- src/main/scala/io/janikdotzel/serialization/Main.scala
+++ src/main/scala/io/janikdotzel/serialization/Main.scala
@@ -5,6 +5,7 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
 import scala.concurrent.Future
 
 
@@ -12,6 +13,8 @@ object Main {
   final case class User(id: String, name: String, age: Int)
   final case class GetUserResponse(success: Boolean)
   final case class CreateUserResponse(success: Boolean)
+
+  var users: Map[String, User] = Map.empty
 
   object JsonFormats {
     // import the default encoders for primitive types (Int, String, Lists etc)
@@ -25,18 +28,25 @@ object Main {
 
 
   def main(args: Array[String]): Unit = {
-
-    implicit val system = ActorSystem(Behaviors.empty, "system")
-    implicit val executionContext = system.executionContext
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "system")
 
     Http().newServerAt("localhost", 8080).bind(routes())
     println(s"Server now online at http://localhost:8080/")
-
   }
 
-  private def getUser(id: String): Future[GetUserResponse] = Future.successful(GetUserResponse(true))
+  private def getUser(id: String): Future[GetUserResponse] = {
+    Future.successful(
+      users.get(id) match {
+        case Some(_) => GetUserResponse(true)
+        case None => GetUserResponse(false)
+      }
+    )
+  }
 
-  private def createUser(user: User): Future[CreateUserResponse] = Future.successful(CreateUserResponse(true))
+  private def createUser(user: User): Future[CreateUserResponse] = {
+    if (!users.contains(user.id)) users = users.+((user.id, user))
+    Future.successful(CreateUserResponse(true))
+  }
 
   private def routes() = {
     // Needed to tell Akka-HTTP to use the Spray Json Support
@@ -44,20 +54,26 @@ object Main {
     import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
     val route = pathPrefix("user") {
-      path(Segment) { id =>
-        concat(
-          get {
-            onSuccess(getUser(id)) { response => complete(response) }
-          },
-          post {
-            // Marshalling works out-of-the-box through the `as[...]` directive
-            entity(as[User]) { user => complete(createUser(user)) }
-          })
-      }
+      concat(
+        post {
+          // Marshalling works out-of-the-box through the `as[...]` directive
+          entity(as[User]) { user => complete(createUser(user)) }
+        },
+        path(Segment) { id =>
+          concat(
+            get {
+              onSuccess(getUser(id)) { response => complete(response) }
+            }
+          )
+        }
+      )
     }
     route
   }
 }
 
-
+/* Test with the following commands
+ Create User: `curl -X POST -H "Content-Type: application/json" -d '{"id":"1", "name":"John Doe", "age":30}' http://localhost:8080/user`
+ Get User: `curl http://localhost:8080/user/1`
+*/
 
