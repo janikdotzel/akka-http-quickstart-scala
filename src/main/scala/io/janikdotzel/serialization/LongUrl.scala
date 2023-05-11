2f3823355cedb72cbba7863dbcdbdc5815d22152
Janik Dotzel <janikdotzel96@gmail.com>
2023-05-11 10:17:13 +0200

    continue longUrl impl

--- src/main/scala/io/janikdotzel/serialization/LongUrl.scala
+++ src/main/scala/io/janikdotzel/serialization/LongUrl.scala
@@ -1,10 +1,14 @@
 package io.janikdotzel.serialization
 
-import akka.actor.typed.ActorSystem
+import akka.actor.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives.{path, _}
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.concurrent.duration.DurationInt
 
 object LongUrl extends App {
 
@@ -12,47 +16,53 @@ object LongUrl extends App {
   val ThreeThousandChars = "X" * 3000
   val TenThousandChars = "X" * 10000
 
-  implicit val system = ActorSystem(Behaviors.empty, "system")
-  implicit val executionContext = system.executionContext
+//  val system = ActorSystem(Behaviors.empty, "system")
+//  implicit val executionContext = system.executionContext
+
+  implicit val system: ActorSystem = ActorSystem("system", ConfigFactory.load())
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+//  println(system.settings.config.getString(""))
 
   Http().newServerAt("localhost", 8080).bind(route)
   println("Server now online at http://localhost:8080/")
 
 
-  val route = concat(
-    pathPrefix("") {
-      get {
-        complete("Server up and running.")
+  def route() =
+    concat(
+      path("") {
+        get {
+          complete("Server up and running.")
+        }
+      },
+      path("test") {
+        get {
+          complete(onTest())
+        }
+      },
+      path(OneThousandChars) {
+        get {
+          complete("success")
+        }
+      },
+      path(ThreeThousandChars) {
+        get {
+          complete("success")
+        }
+      },
+      path(TenThousandChars) {
+        get {
+          complete("success")
+        }
       }
-    },
-    path("test") {
-      get {
-        complete(onTest())
-      }
-    },
-    path(OneThousandChars) {
-      get {
-        complete("success")
-      }
-    },
-    path(ThreeThousandChars) {
-      get {
-        complete("success")
-      }
-    },
-    path(TenThousandChars) {
-      get {
-        complete("success")
-      }
-    }
-  )
+    )
 
-  def onTest(): String = {
-    Http().singleRequest(HttpRequest(uri = "http://localhost:8080/" + OneThousandChars)).onComplete( res => println("1.000 response: " + res))
-    Http().singleRequest(HttpRequest(uri = "http://localhost:8080/" + ThreeThousandChars)).onComplete( res => println("3.000 response: " + res))
-    Http().singleRequest(HttpRequest(uri = "http://localhost:8080/" + TenThousandChars)).onComplete( res => println("10.000 response: " + res))
+  private def onTest(): String = {
+    val req1 = Http().singleRequest(HttpRequest(uri = "http://localhost:8080/" + OneThousandChars))
+    val req3 = Http().singleRequest(HttpRequest(uri = "http://localhost:8080/" + ThreeThousandChars))
+    val req10 = Http().singleRequest(HttpRequest(uri = "http://localhost:8080/" + TenThousandChars))
 
-    "test requests were made"
+    Await.result(req1, 3.seconds) + "\n\n" + Await.result(req3, 3.seconds) + "\n\n" + Await.result(req10, 3.seconds)
   }
 }
 
